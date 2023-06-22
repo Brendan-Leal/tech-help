05f1d7cd85edc7e81a08bc4953ac1d5ff8241ebe
Brendan Leal <leal.brendan@gmail.com>
2023-06-22 14:24:08 -0700

    style adjustments

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,17 @@ import heroImg from "../../public/hero.jpeg"
 
 export default function Home() {
   return (
-    <main className=''>
-      <section className='overflow-hidden sm:grid sm:grid-cols-2'>
-        <div className='p-8 md:p-12 lg:px-16 lg:py-24'>
-          <div className='mx-auto max-w-lg '>
-            <h1 className='text-2xl font-bold md:text-3xl'>Brendan's Tech Help</h1>
-            <p className=''>Local Tech Help Made Easy</p>
-            <div className='mt-4 md:mt-8'>
-              <a href='#' className='inline-block bg-lightBlue rounded text-dark-blue px-8 py-3 '>Schedule an Appointment</a>
-            </div>
+    <main className='bg-dark-blue text-lightBlue'>
+      <section className='overflow-hidden text-center sm:text-left sm:grid sm:grid-cols-2 sm:items-center sm:justify-items-center' >
+        <div className='p-10' >
+          <h1 className='font-spartan text-5xl font-bold sm:text-6xl'>Brendan&apos;s Tech Help</h1>
+          <p className='font-lato font-semibold text-xl'>Local Tech Help Made Easy</p>
+          <div className='mt-4 md:mt-6 font-lato text-lg'>
+            <a href='#' className='inline-block rounded text-dark-blue text-xl px-5 py-3 transition duration-200 hover:ease-in bg-deepOrange hover:scale-110'>Schedule an Appointment</a>
           </div>
         </div>
+
+        {/* Mobile Image */}
         <div className='h-72 w-full relative sm:hidden'>
           <Image
             src="/hero.jpeg"
@@ -23,16 +23,16 @@ export default function Home() {
           />
         </div>
 
+        {/* Tablet size and above Image */}
         <Image
           src="/hero.jpeg"
           alt="Brendan"
-          width={400}
-          height={400}
+          width={450}
+          height={450}
+          style={{ clipPath: 'polygon(20% 0, 100% 0%, 100% 100%, 0 100%)' }}
           className='hidden sm:block object-cover ml-auto'
         />
       </section>
-      <div>
-      </div>
     </main >
   )
 }
