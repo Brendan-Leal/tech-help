29d712ba7a5f1e4d639f61ca0cf127bf9be4ff3f
Brendan Leal <leal.brendan@gmail.com>
2023-06-23 15:03:58 -0700

    update read me, add secret message

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <head
+        dangerouslySetInnerHTML={{ __html: '<!--Website Developed by me, Brendan \nYou can find me at GitHub https://github.com/Brendan-Leal-->' }}
+      >
+      </head>
       <Head />
       <body>
         <Main />
