ab44a0c0e6767b527d2001e3f43ffeb7e640cd62
Brendan Leal <leal.brendan@gmail.com>
2023-06-26 16:07:53 -0700

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,11 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet"></link>
+        <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <div className={`${lato.variable} ${leagueSpartan.variable} font-sans`}>
         <Script defer src='https://assets.calendly.com/assets/external/widget.js' ></Script>
