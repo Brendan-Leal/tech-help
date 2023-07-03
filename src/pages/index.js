1d9c7f28024bc2f5e96622f2ebc1fd2a9fe9cc69
Brendan Leal <leal.brendan@gmail.com>
2023-07-03 14:00:50 -0700

    add pricing section and small adjustments

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,8 +80,74 @@ export default function Home() {
           </div>
         </section>
 
-        {/* Divider
-        <div className='border-t-2 w-11/12 self-center my-5'></div> */}
+        {/* Pricing*/}
+        <section class="bg-dark-blue text-light-blue">
+          <div class="p-10 mx-auto max-w-screen-xl">
+            <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+              <h2 class="mb-4 text-4xl tracking-tight font-extrabold font-spartan">Service Costs</h2>
+              <p class="mb-5 font-lato text-lg">
+                I strive to make the complexities of technology simple and that&apos;s reflected in my simple and straight forward approach to pricing my services.
+              </p>
+            </div>
+            <div class="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0 font-lato text-lg">
+              {/* Pricing Card 1 */}
+              <div class="flex flex-col p-6 mx-auto max-w-lg text-center rounded-lg border border-deep-orange xl:p-8">
+                <h3 class="mb-4 text-2xl font-bold font-spartan">Standard Services</h3>
+                <p class="sm:text-lg">For all your basic tech help needs. Encompassing the most common problems.</p>
+                <div class="flex justify-center items-baseline my-8 text-deep-orange">
+                  <span class="mr-2 text-5xl font-extrabold">$75</span>
+                  <span class="text-gray-500 dark:text-gray-400">flat rate</span>
+                </div>
+                {/* List  */}
+                <ul role="list" class="mb-8 space-y-4 text-left list-disc list-inside ">
+                  <li class="items-center space-x-3 ">
+                    {/* TODO: add Icon?? */}
+                    <span>Troubleshooting Devices</span>
+                  </li>
+                  <li class="items-center space-x-3">
+                    {/* TODO: add Icon?? */}
+                    <span>Cloud Storage</span>
+                  </li>
+                  <li class="items-center space-x-3">
+                    {/* TODO: add Icon?? */}
+                    <span>Device Setups</span>
+                  </li>
+                  <li class="items-center space-x-3">
+                    {/* TODO: add Icon?? */}
+                    <span>Malware Removal</span>
+                  </li>
+                  <li class="items-center space-x-3">
+                    {/* TODO: add Icon?? */}
+                    <span>Computer Optimizations</span>
+                  </li>
+                </ul>
+              </div>
+
+              {/* Pricing Card - Other Services */}
+              <div class="flex flex-col p-6 mx-auto max-w-lg text-center rounded-lg border xl:p-8">
+                <h3 class="mb-4 text-2xl font-bold font-spartan">Other Services</h3>
+                <p class="sm:text-lg mb-20">
+                  I offer other technical services that vary in price or depend on a budget. Get in contact with me and we can discuss the details.
+                </p>
+                <ul role="list" class="mb-8 space-y-4 text-left list-disc list-inside">
+                  <li class="items-center space-x-3">
+                    {/* TODO: add Icon?? */}
+                    <span>Web Development - Please visit <a href='https://astrolabstudio.com' target="_blank" className='underline'>Astrolab Studio</a> to find more information.
+                    </span>
+                  </li>
+                  <li class="items-center space-x-3">
+                    {/* TODO: add Icon?? */}
+                    <span>Custom built PCs</span>
+                  </li>
+                  <li class="items-center space-x-3">
+                    {/* TODO: add Icon?? */}
+                    <span>Have something else in mind? Find my email below.</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
 
         {/* Personal Guarantee */}
         <section className='bg-dark-blue text-light-blue flex justify-center'>
@@ -101,10 +167,11 @@ export default function Home() {
 
           {/* Email Me */}
           <div className='px-10 py-5 max-w-screen-lg xl:mx-auto font-lato text-lg md:text-xl'>
-            <h3 className='text-4xl tracking-tight font-spartan font-extrabold text-dark-blue mb-6'>Have A Questions? Email Me</h3>
+            <h3 className='text-4xl tracking-tight font-spartan font-extrabold text-dark-blue mb-3'>Have A Questions? Email Me</h3>
+            <p className='mb-6'>Don&apos;t see what you&apos;re looking for? Or want to ask me something specific? Reach out to me in an email</p>
             <div className='flex justify-center'>
               {/* */}
-              <a href='mailto:leal.brendan@gmail.com' className='rounded-md text-dark-blue text-xl p-4 max-w-sm w-full  text-center bg-deep-orange transition duration-200 hover:ease-in hover:scale-110'>Send Me an Email</a>
+              <a href='mailto:leal.brendan@gmail.com' className='rounded-md text-dark-blue text-xl p-4 max-w-sm text-center bg-deep-orange transition duration-200 hover:ease-in hover:scale-110'>Send Me an Email</a>
             </div>
           </div>
         </section>
@@ -113,10 +180,10 @@ export default function Home() {
         <section ref={calendlyRef} >
           <div className='px-10 py-5 sm:pb-0 max-w-screen-lg xl:mx-auto font-lato text-lg md:text-xl'>
             <h3 className='text-4xl tracking-tight font-spartan font-extrabold text-dark-blue'>Appointments</h3>
-            <p className='' >Schedule a date and time that works best for you and let&apos;s get your the help you need! I&apos;ll meet you wherever your tech problems live.
+            <p className='' >Schedule a date and time that works best for you and let&apos;s get you the help you need! I&apos;ll meet you wherever your tech problems live.
             </p>
             <br />
-            <p>Or if you want more information you can select the <i>Phone Call</i> option and I&apos;ll give you a call. Just fill out the form below:</p>
+            <p>Or if you want more information you can select the <i>Phone Call</i> option and I&apos;ll give you a call. Simply fill out the form below:</p>
           </div>
           <div className="calendly-inline-widget" data-url="https://calendly.com/leal-brendan/tech-help-appt?hide_gdpr_banner=1" style={{ minWidth: "320px", height: "750px" }}></div>
         </section>
